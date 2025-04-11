727f757168b3498ce46c2845887f74d8844979b7
bosirex <dennis.dancan@gmail.com>
2025-04-11 10:19:31 +0300

    updates

--- myproject/settings.py
+++ myproject/settings.py
@@ -155,4 +155,5 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
